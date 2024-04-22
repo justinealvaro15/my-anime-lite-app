b9116de1e5fb359ac22c13ff9067b09dacac3ce1
Justine Paul Alvaro <jppalvaro@gmail.com>
2024-04-22 21:59:32 +0800

    Chore: Setup react-router-dom package
    - Add React Router DOM package & dependencies
    - Setup RouteProvider in app root to utilize router functionality
    - Add dummy Home Page and remove unused App.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createTheme } from '@mui/material';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/styles';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
 
 const theme = createTheme();
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
