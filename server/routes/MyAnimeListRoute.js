182129dab26f290938d37f6b97ad476969074944
Justine Paul Alvaro <jppalvaro@gmail.com>
2024-04-22 23:49:23 +0800

    New: Setup anime routes
    - Add MyAnimeListRoute to handle calls to the MAL public API
    - Add axios, body-parser, and cors package

---
+++ server/routes/MyAnimeListRoute.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const url = require('url');
+const router = express.Router({ mergeParams: true });
+
+router.post('/token', async (req, res) => {
+    try {
+        const reqInstance = axios.create({
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
+        const params = new url.URLSearchParams(req.body);
+        const response = await reqInstance.post('https://myanimelist.net/v1/oauth2/token', params.toString())
+            .then((response) => res.json(response));
+    } catch (err) {
+        res.json({ message: err.message })
+    }
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const animeRouter = require('./routes/MyAnimeListRoute');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/test", (req, res) => {
-    res.json({ "users": ["user1", "user2", "user3"] })
-});
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api/anime/', animeRouter)
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
